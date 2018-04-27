e45a64d5ad571c65e400c404d0431766a85a386a
Ricardo <eehaha@gmail.com>
2018-04-28 00:13:17 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const http = require("http");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const app = express();
+const router = require("./router");
+const mongoose = require("mongoose");
+
+//DB Set up
+mongoose.connect("mongodb://localhost/auth");
+
+// App Set up
+app.use(morgan("combined"));
+app.use(bodyParser.json({ type: "*/*" }));
+router(app);
+// Server setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log("Server Listening on:", port);
